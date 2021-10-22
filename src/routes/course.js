ebed6cfe1407af4f3e5457a62b037be39797f0b6
ccdosorio <ccdosorio11@gmail.com>
2021-10-22 11:32:01 -0600

    Print error

--- src/routes/course.js
+++ src/routes/course.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.get("/cursos", (req, res) => {
     let query = "SELECT * FROM curso;";
     conn.query(query, (err, rows) => {
-      if (err) res.status(500).json({ status: 1, message: "Error", data: [] });
+      if (err) res.status(500).json({ status: 1, message: err, data: [] });
       else res.json({ status: 0, message: "Ok", data: rows });
     });
   });
