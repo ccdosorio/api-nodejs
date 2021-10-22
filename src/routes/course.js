5ad99c642fc4216f47a0a1555cb59368a978a0f6
ccdosorio <ccdosorio11@gmail.com>
2021-10-22 11:51:16 -0600

    pool edit

--- src/routes/course.js
+++ src/routes/course.js
@@ -1,11 +1,21 @@
 const conn = require("../config/database");
 
 module.exports = (app) => {
-  app.get("/cursos", (req, res) => {
+  app.get("/cursos", async (req, res) => {
     let query = "SELECT * FROM curso;";
-    conn.query(query, (err, rows) => {
-      if (err) res.status(500).json({ status: 1, message: err, data: [] });
-      else res.json({ status: 0, message: "Ok", data: rows });
-    });
+    const connection = await conn.getConnection();
+    const data = await connection.query(query);
+    let results = data[0];
+
+    try {
+      return res.json({ status: 0, message: "Ok", data: results });
+    } catch (error) {
+      res.status(500).json(err);
+    }
+
+    // connection.query(query, (err, rows) => {
+    //   if (err) res.status(500).json({ status: 1, message: err, data: [] });
+    //   else res.json({ status: 0, message: "Ok", data: rows });
+    // });
   });
 };
