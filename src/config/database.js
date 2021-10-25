c3ce34bc1e74b6bf7d52d7bcff5f95c4485d8d92
ccdosorio <ccdosorio11@gmail.com>
2021-10-24 23:33:27 -0600

    configuración RDS

--- src/config/database.js
+++ src/config/database.js
@@ -1,28 +1,7 @@
-// const mysql = require("mysql");
-// require("dotenv").config();
-
-// var conn = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   port: process.env.DB_PORT,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_DATABASE,
-// });
-
-// conn.connect((err) => {
-//   if (err) {
-//     console.log("Database connection error");
-//   } else {
-//     console.log("Successful connection to the database");
-//   }
-// });
-
-// module.exports = conn;
-const mysql2 = require("mysql2/promise");
+const mysql = require("mysql");
 require("dotenv").config();
 
-var conn = mysql2.createPool({
-  connectionLimit: 15,
+var conn = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: process.env.DB_PORT,
@@ -30,25 +9,12 @@ var conn = mysql2.createPool({
   database: process.env.DB_DATABASE,
 });
 
-async function getPool() {
-  const connection = await conn.getConnection();
-  try {
-    const data = await connection.query("SELECT 1 + 1 AS solution");
-    return data;
-  } catch (error) {
-    console.log(error);
+conn.connect((err) => {
+  if (err) {
+    console.log("Database connection error");
+  } else {
+    console.log("Successful connection to the database");
   }
-}
-
-async function main() {
-  try {
-    const results = await getPool();
-    console.log("The solution is: " + results[0][0].solution);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-main();
+});
 
 module.exports = conn;
--- src/routes/course.js
+++ src/routes/course.js
@@ -1,21 +1,11 @@
 const conn = require("../config/database");
 
 module.exports = (app) => {
-  app.get("/cursos", async (req, res) => {
+  app.get("/cursos", (req, res) => {
     let query = "SELECT * FROM curso;";
-    const connection = await conn.getConnection();
-    const data = await connection.query(query);
-    let results = data[0];
-
-    try {
-      return res.json({ status: 0, message: "Ok", data: results });
-    } catch (error) {
-      res.status(500).json(err);
-    }
-
-    // connection.query(query, (err, rows) => {
-    //   if (err) res.status(500).json({ status: 1, message: err, data: [] });
-    //   else res.json({ status: 0, message: "Ok", data: rows });
-    // });
+    conn.query(query, (err, rows) => {
+      if (err) res.status(500).json({ status: 1, message: "Error", data: [] });
+      else res.json({ status: 0, message: "Ok", data: rows });
+    });
   });
 };
