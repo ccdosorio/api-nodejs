85a11cb8a0af3b3c716b32fe6f7314ac0f3612d7
ccdosorio <ccdosorio11@gmail.com>
2021-10-24 23:36:35 -0600

    bug rds

--- src/routes/career.js
+++ src/routes/career.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.get("/carreras", (req, res) => {
     let query = "SELECT * FROM carrera;";
     conn.query(query, (err, rows) => {
-      if (err) res.status(500).json({ status: 1, message: "Error", data: [] });
+      if (err) res.status(500).json({ status: 1, message: err, data: [] });
       else res.json({ status: 0, message: "Ok", data: rows });
     });
   });
